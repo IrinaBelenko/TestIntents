75977a67110535efa8960e46df84aff23ea53605
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2024-01-20 16:23:48 +0200

    return to first activity

--- app/src/main/java/com/example/testintents/MainActivity.kt
+++ app/src/main/java/com/example/testintents/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +22,17 @@ class MainActivity : Activity() {
 //            startActivity(actionIntent)
             //3
             val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("SecondActivityMessage", "Message from first activity")
-            startActivity(intent)
+            intent.putExtra(MESSAGE_TO_SECOND_ACTIVITY, "Message from first activity")
+            startActivityForResult(intent,600)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+
+        if (resultCode == RESULT_OK && requestCode == 600) {
+            val message = intent?.getStringExtra(SecondActivity.MESSAGE_TO_FIRST_ACTIVITY)
+            message?.let {
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
--- app/src/main/java/com/example/testintents/SecondActivity.kt
+++ app/src/main/java/com/example/testintents/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.example.testintents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -11,5 +13,18 @@ class SecondActivity : AppCompatActivity() {
         val message: TextView = findViewById(R.id.title)
         intent?.let { message.text =
             it.getStringExtra(MainActivity.MESSAGE_TO_SECOND_ACTIVITY) }
+
+        val sendBackButton:Button = findViewById(R.id.sendBackButton)
+        sendBackButton.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra(MESSAGE_TO_FIRST_ACTIVITY,"Message From Second Activity")
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
+    }
+
+    companion object {
+        const val MESSAGE_TO_FIRST_ACTIVITY = "messageToFirstActivity"
     }
 }
